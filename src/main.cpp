bb0292f52a006a94d26f84961aa0ccf9bb3f6973
Taisyu Shibata <t_shiba117@yahoo.co.jp>
2023-12-15 02:14:29 +0900

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,64 @@
 /* Author: Taisyu Shibata */
 
-#include "rclcpp/rclcpp.hpp"
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/int32_multi_array.hpp>
 #include <pigpio.h>
+#include <iostream>
+#include <chrono>
+#include <thread>
 
-int main(int argc, char** argv)
+#define bottom 50
+#define R 12
+#define L 13
+#define ENABLE_r 17
+#define ENABLE_l 18
+
+class SubscriberNode : public rclcpp::Node
 {
-  rclcpp::init(argc, argv);
-//  rclcpp::spin();
-  rclcpp::shutdown();
-  return 0;
+public:
+    SubscriberNode() : Node("subscriber"), joy_r(0), joy_l(0)
+    {
+        if (gpioInitialise() < 0) {
+            RCLCPP_ERROR(this->get_logger(), "Failed to initialize GPIO");
+            rclcpp::shutdown();
+        }
+
+        gpioSetMode(R, PI_OUTPUT);
+        gpioSetMode(L, PI_OUTPUT);
+        gpioSetMode(ENABLE_r, PI_OUTPUT);
+        gpioSetMode(ENABLE_l, PI_OUTPUT);
+
+        gpioWrite(ENABLE_r, PI_LOW);
+        gpioWrite(ENABLE_l, PI_LOW);
+
+        // GPIO PWM setup code here
+
+        subscription_ = this->create_subscription<std_msgs::msg::Int32MultiArray>(
+            "verocity", 10, std::bind(&SubscriberNode::toGpio, this, std::placeholders::_1));
+    }
+
+    ~SubscriberNode() {
+        gpioTerminate(); // Clean up pigpio library
+    }
+
+private:
+    void toGpio(const std_msgs::msg::Int32MultiArray::SharedPtr msg)
+    {
+        joy_r = msg->data[0];
+        joy_l = msg->data[1];
+        // GPIO操作のロジックをここに実装します
+    }
+
+    rclcpp::Subscription<std_msgs::msg::Int32MultiArray>::SharedPtr subscription_;
+    int joy_r, joy_l;
+};
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<SubscriberNode>();
+    rclcpp::spin(node);
+    node = nullptr;
+    rclcpp::shutdown();
+    return 0;
 }
