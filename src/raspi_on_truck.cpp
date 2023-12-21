276fec36bdb17087f35d91f2a32e522eb1012d11
Taisyu Shibata <t_shiba117@yahoo.co.jp>
2023-12-22 06:36:37 +0900

    Edit

--- src/raspi_on_truck.cpp
+++ src/raspi_on_truck.cpp
@@ -76,10 +76,12 @@ private:
     int joy_r, joy_l;
 };
 
+std::shared_ptr<SubscriberNode> node = nullptr;
+
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<SubscriberNode>();
+    node = std::make_shared<SubscriberNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
