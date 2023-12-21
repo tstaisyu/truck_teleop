906b17be9b3a3779f37136500846d5c55b3011a6
Taisyu Shibata <t_shiba117@yahoo.co.jp>
2023-12-22 06:39:24 +0900

    Edit

--- src/raspi_on_truck.cpp
+++ src/raspi_on_truck.cpp
@@ -51,6 +51,7 @@ private:
     void toGpio(const std_msgs::msg::Int32MultiArray::SharedPtr msg)
     {
         RCLCPP_INFO(this->get_logger(), "toGpio callback called.");
+
         if (!msg) {
             RCLCPP_ERROR(this->get_logger(), "Received null pointer in callback");
             return;
@@ -63,11 +64,10 @@ private:
 
         joy_r = msg->data[0];
         joy_l = msg->data[1];
+        RCLCPP_INFO(this->get_logger(), "Right Joystick: %d, Left Joystick: %d", joy_r, joy_l);
 
         // Implement GPIO operations logic here
-        RCLCPP_INFO(this->get_logger(), "Right Joystick: %d, Left Joystick: %d", joy_r, joy_l);
-        
-        // Example: Set GPIO pin values based on joystick input
+        // Example: Adjust the PWM values based on joystick input
         // gpioPWM(R, joy_r); // Adjust as necessary
         // gpioPWM(L, joy_l); // Adjust as necessary
     }
